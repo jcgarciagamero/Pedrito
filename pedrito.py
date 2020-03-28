ed441a73cdb5e6f7267dc9692c1f4f7c204cf6b1
José C. García Gamero <jcgarciagamero@gmail.com>
2020-03-28 11:38:32 +0100

    Update pedrito.py

--- pedrito.py
+++ pedrito.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
-#Version: 1.0.0
+#Version: 1.0.1
 #Created by Jose C. García Gamero
 #Twitter: @jcgarciagamero
 
 import os
 import sys
-import urllib, re
+import urllib.request, urllib.parse, urllib.error, re
 
 print('  ____   U _____ u____    ____                 _____   U  ___ u ')
-print('U|  _"\ u\| ___"|/  _"\U |  _"\ u     ___     |_ " _|   \/"_ \/ ')
+print('U|  _"\ u\| ___"|/  _"\\U |  _"\ u     ___     |_ " _|   \/"_ \/ ')
 print('\| |_) |/ |  _|"/| | | |\| |_) |/    |_"_|      | |     | | | | ')
 print(' |  __/   | |___U| |_| |\|  _ <       | |      /| |\.-,_| |_| | ')
-print(' |_|      |_____||____/ u|_| \_\    U/| |\u   u |_|U \_)-\___/  ')
+print(' |_|      |_____||____/ u|_| \_\    U/| |\\u   u |_|U \_)-\___/  ')
 print(' ||>>_    <<   >> |||_   //   \\_.-,_|___|_,-._// \\_     \\    ')
 print('(__)__)  (__) (__|__)_) (__)  (__)\_)-' '-(_/(__) (__)   (__)   ')
 
@@ -24,15 +24,16 @@ for enlace in enlaces:
 
 	var = 0
 	scan = enlace
-	os.system("@echo off")
-	os.system("curl -L -m 10 %s > tmp.html" %scan)
-	code = "tmp.html"
-	lines = urllib.urlopen(code).read()
+	os.system("curl -L -m 10 %s > tmp.txt" %scan)
+	
+	f = open ('tmp.txt','r',encoding='utf-8')
+	lines = f.read()
+	f.close()
 
 	
 	
 	words = (['keyword','keyword2']) #Modify with your list of keywords
-	print("Domain: "+enlace)
+	print(("Domain: "+enlace))
 	print("Detected keywords:")
 
 	for busqueda in words:
@@ -50,7 +51,7 @@ for enlace in enlaces:
 				f.close()
 				print("Links: ")
 				print(enlas)
-			  	f=open("logs.txt","a")
+				f=open("logs.txt","a")
 				f.write("\n")
 				f.close()
 
@@ -63,17 +64,3 @@ for enlace in enlaces:
 				f.write("\n")
 				f.close()
 		
-
-	
-		
-    
-				
-		
-
-
-
-    
-				
-		
-
-
